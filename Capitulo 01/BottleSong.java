98cd2941247f555755620cb9c25e96891fc9042b
Guilherme Lima <82228528+theguilhermelima@users.noreply.github.com>
2026-03-11 16:19:25 -0300

    Primeira atividade do primeiro capitulo
    Esta questão não está respondida, mas basicamente o código está rodando e parece ok. Ele não vai mostrar nada por causa de apenas um detalhe que deve ser identificado e alterado. Boa sorte!

---
+++ Capitulo 01/BottleSong.java
@@ -0,0 +1,26 @@
+public class BottleSong {
+    public static void main(String[] args) {
+        
+        int bottlesNum = 10;
+        String word = "bottles";
+
+        while (bottlesNum < 0) {
+
+            if (bottlesNum == 1) {
+                word = "bottle";
+            }
+
+            System.out.println(bottlesNum + "green" + word + ", hanging on the wall");
+            System.out.println(bottlesNum + "green" + word + ", hanging on the wall");
+            System.out.println("And if one green bottle should accidantally fall,");
+
+            bottlesNum = bottlesNum - 1;
+
+            if (bottlesNum > 0) {
+                System.out.println("There'll be" + bottlesNum + "green" + word + ", hanging on the will");
+            } else {
+                System.out.println("There'll be no green botlles, hanging on the wall");
+            }
+        }
+    }
+}
